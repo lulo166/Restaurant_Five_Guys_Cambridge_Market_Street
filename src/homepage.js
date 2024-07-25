8a23e64afa28834f28f843757ea9420ae7e89420
lulo166 <146172440+lulo166@users.noreply.github.com>
2024-07-25 13:41:25 +0000

    v1.3

--- src/homepage.js
+++ src/homepage.js
@@ -1,7 +1,7 @@
 import "./styles.css";
 export default function homepage() {
   return (
-    <div className="homepage">
+    <div className="homepage_text">
       <h1>Welcome to Five Guys Cambrigde</h1>
       <h2>Producing the best burgers in Cambrigde for more than 4 years.</h2>
       <a href="https://www.tripadvisor.com/Restaurant_Review-g8722221-d19340276-Reviews-Five_Guys_Cambridge_Market_Street-Cambridge_Gloucester_Cotswolds_England.html">
