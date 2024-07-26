69938598980d1b869407fd195b0db97048144479
lulo166 <146172440+lulo166@users.noreply.github.com>
2024-07-26 07:38:47 +0000

    orders

--- src/Menu_and_Orders.js
+++ src/Menu_and_Orders.js
@@ -1,18 +1,20 @@
 import "./styles.css";
+import Mycard from "./Mycard";
+
 export default function Menu_and_Orders() {
   return (
   <div className="cardbox">
-  <Mycard title="1" imgLocation="images/King's College" />
-  <Mycard title="1" imgLocation="images/King's College" />
-  <Mycard title="1" imgLocation="images/King's College" />
-  <Mycard title="1" imgLocation="images/King's College" />
-  <Mycard title="1" imgLocation="images/King's College" />
-  <Mycard title="1" imgLocation="images/King's College" />
-  <Mycard title="1" imgLocation="images/King's College" />
-  <Mycard title="1" imgLocation="images/King's College" />
-  <Mycard title="1" imgLocation="images/King's College" />
-  <Mycard title="1" imgLocation="images/King's College" />
-  <Mycard title="1" imgLocation="images/King's College" />
+    <Mycard title="Hamburger" imgLocation="images/Hamburger.jpg" />
+    <Mycard title="Cheeseburger" imgLocation="" />
+    <Mycard title="Bacon Burger" imgLocation="" />
+    <Mycard title="Bacon Cheeseburger" imgLocation="" />
+    <Mycard title="Five Guys Style Fries" imgLocation="" />
+    <Mycard title="Cajun Style Fries" imgLocation="" />
+    <Mycard title="Coca Cola" imgLocation="" />
+    <Mycard title="Sprite" imgLocation="" />
+    <Mycard title="Fanta" imgLocation="" />
+    <Mycard title="DrPepper" imgLocation="" />
+    <Mycard title="Water" imgLocation="" />
 </div>
 );
 }
--- src/Mycard.js
+++ src/Mycard.js
@@ -2,23 +2,22 @@ import "./styles.css";
 import { useState } from "react";
 
 export default function Mycard(props) {
-  const [order, setLike] = useState(0);
-  function handleLike() {
-    setLike(order + 1);
-    props.setTotalLike(props.totalLike + 1);
+  const [order, setOrder] = useState(0);
+  function handleOrder() {
+    setOrder(order + 1);
+    props.setTotalLike(props.totalOrder + 1);
   }
-  function handledisLike() {
-    setLike(order - 1);
+  function handleLessOrder() {
+    setOrder(order - 1);
   }
 
   return (
     <div className="mycard">
-      <h4>My Card</h4>
       <h5>{props.title}</h5>
       <img src={props.imgLocation} width="100px" height="100px" />
-      <div>Like: {like}</div>
-      <button onClick={handleLike}>Like</button>
-      <button onClick={handledisLike}>Dislike</button>
+      <div>order: {order}</div>
+      <button onClick={handleOrder}>Add One Item</button>
+      <button onClick={handleLessOrder}>Remove One Item</button>
     </div>
   );
 }
