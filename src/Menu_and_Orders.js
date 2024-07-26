9e5d739cbe9dcc18c5b48f12e2d7e43fd29c1d31
lulo166 <146172440+lulo166@users.noreply.github.com>
2024-07-26 08:18:50 +0000

    faire les boissons

--- src/Menu_and_Orders.js
+++ src/Menu_and_Orders.js
@@ -7,8 +7,8 @@ export default function Menu_and_Orders() {
     <Mycard title="Hamburger" imgLocation="images/Hamburger.jpg" />
     <Mycard title="Cheeseburger" imgLocation="images/Cheeseburger.jpg" />
     <Mycard title="Bacon Cheeseburger" imgLocation="images/Bacon_cheeseburger.jpg" />
-    <Mycard title="Five Guys Style Fries" imgLocation="images/" />
-    <Mycard title="Cajun Style Fries" imgLocation="images/" />
+    <Mycard title="Five Guys Style Fries" imgLocation="images/fries.jpg" />
+    <Mycard title="Cajun Style Fries" imgLocation="images/five-guys-cajun-fries.jpg" />
     <Mycard title="Coca Cola" imgLocation="images/" />
     <Mycard title="Sprite" imgLocation="images/" />
     <Mycard title="Fanta" imgLocation="images/" />
