3a3b53348820fce3eadae47dbd7514876469a737
lulo166 <146172440+lulo166@users.noreply.github.com>
2024-07-26 08:42:49 +0000

    v

--- src/Menu_and_Orders.js
+++ src/Menu_and_Orders.js
@@ -3,16 +3,17 @@ import Mycard from "./Mycard";
 
 export default function Menu_and_Orders() {
   return (
+
   <div className="cardbox">
     <Mycard title="Hamburger" imgLocation="images/Hamburger.jpg" />
     <Mycard title="Cheeseburger" imgLocation="images/Cheeseburger.jpg" />
     <Mycard title="Bacon Cheeseburger" imgLocation="images/Bacon_cheeseburger.jpg" />
     <Mycard title="Five Guys Style Fries" imgLocation="images/fries.jpg" />
     <Mycard title="Cajun Style Fries" imgLocation="images/five-guys-cajun-fries.jpg" />
-    <Mycard title="Coca Cola" imgLocation="images/" />
-    <Mycard title="Sprite" imgLocation="images/" />
-    <Mycard title="Fanta" imgLocation="images/" />
-    <Mycard title="Water" imgLocation="images/" />
-</div>
+    <Mycard title="Coca Cola" imgLocation="images/coca-cola.jpg" />
+    <Mycard title="Sprite" imgLocation="images/Sprite_logo.webp" />
+    <Mycard title="Fanta" imgLocation="images/Fanta-Logo.png" />
+    <Mycard title="Water" imgLocation="images/water.jpg" />
+  </div>
 );
 }
